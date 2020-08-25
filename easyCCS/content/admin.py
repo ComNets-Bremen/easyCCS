39e89ebb81623c11de266b16f8da57dfe4c20faf
Jens Dede <jd@comnets.uni-bremen.de>
2020-08-25 23:19:31 +0200

    Add link property, rm alias for skill

--- easyCCS/content/admin.py
+++ easyCCS/content/admin.py
@@ -1,30 +1,24 @@
 from django.contrib import admin
 from django.db.models import Q
 
-from .models import Skill, Content, Module
-
-class SkillAdmin(admin.ModelAdmin):
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        self.object_id = object_id
-        return super().change_view(
-            request, object_id, form_url, extra_context=extra_context,
-        )
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        # Exclude self for foreign key
-        if db_field.name == "is_alias_for":
-            if hasattr(self, "object_id"):
-                kwargs["queryset"] =  Skill.objects.filter(is_alias_for=None).exclude(pk=self.object_id)
-            else:
-                kwargs["queryset"] =  Skill.objects.filter(is_alias_for=None)
-
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+from .models import Skill, Content, Module, LinkProperty
 
 
 class ContentAdmin(admin.ModelAdmin):
     readonly_fields = ["created", "updated"]
     save_as = True
+    autocomplete_fields = ["required_skills", "new_skills"]
+
+
+class LinkPropertyAdmin(admin.ModelAdmin):
+    readonly_fields = ["properties",]
+
+
+class SkillAdmin(admin.ModelAdmin):
+    ordering = ["-id"]
+    search_fields = ["skill_name", "skill_descriptive_keywords"]
 
 admin.site.register(Skill, SkillAdmin)
 admin.site.register(Content, ContentAdmin)
 admin.site.register(Module)
+admin.site.register(LinkProperty, LinkPropertyAdmin)
