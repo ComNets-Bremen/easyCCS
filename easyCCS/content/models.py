934042fb7dee9bb2b0b7e8721316f64dfa5c12ce
Jens Dede <jd@comnets.uni-bremen.de>
2020-08-26 16:04:49 +0200

    Improved formatting

--- easyCCS/content/models.py
+++ easyCCS/content/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.conf import settings
 from django.db.models.signals import post_delete
+from django.utils.translation import gettext_lazy as _
 
 import os
 import uuid
